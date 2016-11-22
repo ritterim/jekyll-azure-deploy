d0c6a68750184310649a7e28b9108ab32791d289
Ken Dale <ken@kendaleiv.com>
2016-11-22 11:10:56 -0500

    Update Ruby and RubyGems versions

--- getruby.cmd
+++ getruby.cmd
@@ -2,7 +2,7 @@
 
 REM Put Ruby in Path
 REM You can also use %TEMP% but it is cleared on site restart. Tools is persistent.
-SET PATH=%PATH%;D:\home\site\deployments\tools\r\ruby-2.2.4-x64-mingw32\bin
+SET PATH=%PATH%;D:\home\site\deployments\tools\r\ruby-2.3.1-x64-mingw32\bin
 
 REM I am in the repository folder
 pushd D:\home\site\deployments
@@ -10,16 +10,16 @@ if not exist tools md tools
 cd tools 
 if not exist r md r
 cd r 
-if exist ruby-2.2.4-x64-mingw32 goto end
+if exist ruby-2.3.1-x64-mingw32 goto end
 
 echo No Ruby, need to get it!
 
 REM Get Ruby and Rails
 REM 64bit
-curl -o ruby224.zip -L https://bintray.com/artifact/download/oneclick/rubyinstaller/ruby-2.2.4-x64-mingw32.7z?direct
+curl -o ruby231.zip -L https://bintray.com/artifact/download/oneclick/rubyinstaller/ruby-2.3.1-x64-mingw32.7z?direct
 REM Azure puts 7zip here!
 echo START Unzipping Ruby
-SetLocal DisableDelayedExpansion & d:\7zip\7za x -xr!*.ri -y ruby224.zip > rubyout
+SetLocal DisableDelayedExpansion & d:\7zip\7za x -xr!*.ri -y ruby231.zip > rubyout
 echo DONE Unzipping Ruby
 
 REM Get DevKit to build Ruby native gems  
@@ -34,13 +34,13 @@ ruby DevKit\dk.rb init
 
 REM Tell DevKit where Ruby is
 echo --- > config.yml
-echo - D:/home/site/deployments/tools/r/ruby-2.2.4-x64-mingw32 >> config.yml
+echo - D:/home/site/deployments/tools/r/ruby-2.3.1-x64-mingw32 >> config.yml
 
 REM Setup DevKit
 ruby DevKit\dk.rb install
 
-REM Update Gem223 until someone fixes the Ruby Windows installer https://github.com/oneclick/rubyinstaller/issues/261
-curl -L -o update.gem https://github.com/rubygems/rubygems/releases/download/v2.2.3/rubygems-update-2.2.3.gem
+REM Update until someone fixes the Ruby Windows installer https://github.com/oneclick/rubyinstaller/issues/261
+curl -L -o update.gem https://rubygems.org/gems/rubygems-update-2.6.8.gem
 call gem install --local update.gem
 call update_rubygems --no-ri --no-rdoc > updaterubygemsout
 ECHO What's our new Rubygems version?
